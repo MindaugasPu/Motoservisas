f6f9b73f66563dc0eb0e2223dc5c056196fd0002
MindaugasPu <mindaugas.pupienis@gmail.com>
2023-01-18 20:46:42 +0200

    sutvarkytas admin puslapis su užsakymų sumomis

--- mysite/motoservice/admin.py
+++ mysite/motoservice/admin.py
@@ -1,9 +1,30 @@
 from django.contrib import admin
 from .models import Motociklas, MotocikloModelis, Uzsakymas, UzsakymoEilute, Paslauga
 
+class MotociklasAdmin(admin.ModelAdmin):
+    list_display = ('motociklo_modelis', 'metai', 'valstybinis_NR', 'vin_kodas', 'klientas')
+    list_filter = ('klientas', 'motociklo_modelis')
+    search_fields = ('valstybinis_NR', 'vin_kodas')
+
+class UzsakymoEiltueInLine(admin.TabularInline):
+    model = UzsakymoEilute
+    extra = 0
+
+class UzsakymasAdmin(admin.ModelAdmin):
+    list_display = ('motociklas', 'data', 'suma')
+    inlines = [UzsakymoEiltueInLine]
+
+class UzsakymoEiltueAdmin(admin.ModelAdmin):
+    list_display = ('uzsakymas', 'paslauga', 'kiekis', 'kaina')
+    list_filter = ('paslauga',)
+    search_fields = ('uzsakymas',)
+
+class PaslaugaAdmin(admin.ModelAdmin):
+    list_display = ('pavadinimas', 'kaina')
+
 # Register your models here.
-admin.site.register(Motociklas)
+admin.site.register(Motociklas, MotociklasAdmin)
 admin.site.register(MotocikloModelis)
-admin.site.register(Uzsakymas)
-admin.site.register(UzsakymoEilute)
-admin.site.register(Paslauga)
+admin.site.register(Uzsakymas, UzsakymasAdmin)
+admin.site.register(UzsakymoEilute, UzsakymoEiltueAdmin)
+admin.site.register(Paslauga, PaslaugaAdmin)
--- mysite/motoservice/models.py
+++ mysite/motoservice/models.py
@@ -9,6 +9,10 @@ class MotocikloModelis(models.Model):
     def __str__(self):
         return f"{self.marke} {self.modelis}"
 
+    class Meta:
+        verbose_name = 'Motociklo modelis'
+        verbose_name_plural = 'Motociklų modeliai'
+
 
 class Motociklas(models.Model):
     valstybinis_NR = models.CharField('Valstybiniai numeriai', max_length=7)
@@ -18,29 +22,52 @@ class Motociklas(models.Model):
     metai = models.IntegerField("Metai")
 
     def __str__(self):
-        return f"{self.metai} {self.motociklo_modelis} [{self.valstybinis_NR}] - {self.klientas}"
+        return f"{self.metai} {self.motociklo_modelis} [{self.valstybinis_NR}]"
 
+    class Meta:
+        verbose_name = 'Motociklas'
+        verbose_name_plural = 'Motociklai'
 
 class Uzsakymas(models.Model):
     data = models.DateTimeField("Data", auto_now_add=True)
     motociklas = models.ForeignKey(to="Motociklas", on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"[{self.motociklas.valstybinis_NR}] {self.motociklas.motociklo_modelis} -- {self.data}"
+        return f"[{self.motociklas.valstybinis_NR}] {self.motociklas.motociklo_modelis}"
 
+    def suma(self):
+        suma = 0
+        eilutes = self.eilutes.all()
+        for eilute in eilutes:
+            suma += eilute.kaina()
+        return suma
+
+
+    class Meta:
+        verbose_name = 'Užsakymas'
+        verbose_name_plural = 'Užsakymai'
 
 class UzsakymoEilute(models.Model):
-    uzsakymas = models.ForeignKey(to="Uzsakymas", on_delete=models.CASCADE)
+    uzsakymas = models.ForeignKey(to="Uzsakymas", on_delete=models.CASCADE, related_name="eilutes")
     paslauga = models.ForeignKey(to="Paslauga", on_delete=models.SET_NULL, null=True)
     kiekis = models.IntegerField("Kiekis")
 
     def __str__(self):
         return f"{self.uzsakymas.motociklas.motociklo_modelis} {self.paslauga.pavadinimas}"
 
+    def kaina(self):
+        return self.paslauga.kaina * self.kiekis
+    class Meta:
+        verbose_name = 'Užsakymo eilutė'
+        verbose_name_plural = 'Užsakymų eilutės'
 
 class Paslauga(models.Model):
     pavadinimas = models.CharField('Pavadinimas', max_length=200)
     kaina = models.IntegerField('Kaina')
 
     def __str__(self):
-        return f"{self.pavadinimas} {self.kaina}"
+        return f"{self.pavadinimas}"
+
+    class Meta:
+        verbose_name = 'Paslauga'
+        verbose_name_plural = 'Paslaugos'
